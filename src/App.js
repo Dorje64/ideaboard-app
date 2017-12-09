1971422cb96269ae5e49029341246ebe962893b4
Dorje <ku.dorjee@gmail.com>
2017-12-09 23:34:20 +0545

    Create new Idea Add new Idea button Make Axios call to create idea

--- src/App.js
+++ src/App.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import IdeasContainer from './components/IdeasContainer.js.jsx'
+import Axios from 'axios'
 
 class App extends Component {
+  newIdea = () =>{
+    Axios.post('http://localhost:3001/api/v1/ideas',
+    {idea:
+      {
+        title: '',
+        body: ''
+      }
+    })
+    .then(response =>
+      {
+        console.log(response)
+      }
+    ).catch( error =>
+      {
+        console.log(error)
+      }
+    )
+
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +32,9 @@ class App extends Component {
           <h1 className="App-title">IdeaBroad</h1>
           <h1 className="App-sub-title">Write your idea on IdeaBroad</h1>
         </header>
-        <div className="blue-subheader"></div>
+        <div className="blue-subheader">
+          <button className="newIdeaButton" onClick={this.newIdea}> New Idea </button>
+        </div>
        <IdeasContainer />
       </div>
     );
