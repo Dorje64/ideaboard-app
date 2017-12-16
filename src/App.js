e110b37c58750bbd1633093331b8453310f9660a
Dorje <ku.dorjee@gmail.com>
2017-12-16 12:37:45 +0545

    move newIdea button and pist axios call to ideascontainer

--- src/App.js
+++ src/App.js
@@ -5,25 +5,6 @@ import IdeasContainer from './components/IdeasContainer.js.jsx'
 import Axios from 'axios'
 
 class App extends Component {
-  newIdea = () =>{
-    Axios.post('http://localhost:3001/api/v1/ideas',
-    {idea:
-      {
-        title: '',
-        body: ''
-      }
-    })
-    .then(response =>
-      {
-        console.log(response)
-      }
-    ).catch( error =>
-      {
-        console.log(error)
-      }
-    )
-
-  }
 
   render() {
     return (
@@ -32,9 +13,7 @@ class App extends Component {
           <h1 className="App-title">IdeaBroad</h1>
           <h1 className="App-sub-title">Write your idea on IdeaBroad</h1>
         </header>
-        <div className="blue-subheader">
-          <button className="newIdeaButton" onClick={this.newIdea}> New Idea </button>
-        </div>
+
        <IdeasContainer />
       </div>
     );
