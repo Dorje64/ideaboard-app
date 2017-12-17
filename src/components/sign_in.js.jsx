0e68782e5e4f598db9d8674117acd04265094280
Dorje <ku.dorjee@gmail.com>
2017-12-17 22:18:43 +0545

    start login form

---
+++ src/components/sign_in.js.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import Axios from 'axios'
+
+export default class SignIn extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  handleInput = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.priventDefault();
+    Axios.post()
+    .then()
+    .catch();
+  }
+
+  render(){
+    return(
+      <div>
+        <h1> Sign In</h1>
+        <form onSubmit= {this.handleSubmit}>
+          <input type="text" name="email" placeholder="Email" onChange={this.handleInput}/>
+          <input type="password" name="password" placeholder="Password" onChange={this.handleInput}/>
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    )
+  }
+}
