9ea21b4229bd4bdb6290a10c38d9363f80c2d1dd
Dorje <ku.dorjee@gmail.com>
2017-12-17 12:03:30 +0545

    add form and handle submit event

--- src/components/idea_form_component.js.jsx
+++ src/components/idea_form_component.js.jsx
@@ -1,21 +1,44 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import Axios from 'axios'
 
 class IdeaFormComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      title: this.props.idea.title,
+      body: this.props.idea.body
     }
+  }
+
+  handleInput = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
+  SubmitForm = (e) =>{
+    e.preventDefault();
+    let idea = {
+      title: this.state.title,
+      body: this.state.body
+    }
+    Axios.put('http://localhost:3001/api/v1/ideas/'+String(this.props.idea.id),
+            {idea: idea}
+          )
+          .then(response => {
+            console.log(response)
+            this.props.updateIdeas(response.data)
+          })
+          .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div className="tile">
-        <form>
+        <form onSubmit= {this.SubmitForm} >
           <input className='input' type="text"
-            name="title" placeholder='Enter a Title' />
+            name="title" placeholder='Enter a Title' onChange={this.handleInput} />
           <textarea className='input' name="body"
-            placeholder='Describe your idea'></textarea>
+            placeholder='Describe your idea' onChange={this.handleInput} ></textarea>
+            <input type="submit" value="Submit"/>
         </form>
       </div>
     );
