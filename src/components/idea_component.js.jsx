b19603ad1d3b3020e422c4fdd28a135508490945
Dorje <ku.dorjee@gmail.com>
2017-12-17 12:01:49 +0545

    edit option to idea

--- src/components/idea_component.js.jsx
+++ src/components/idea_component.js.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const IdeaComponent = ({idea}) =>
+const IdeaComponent = ({idea, enableEdit}) =>
               <div className="tile" key={idea.id} >
                 <h4>{idea.title}</h4>
                 <p>{idea.body}</p>
+                <p onClick= {enableEdit(idea.id)}> Edit </p>
               </div>
 
 export default IdeaComponent
