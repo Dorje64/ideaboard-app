673eeda470a3c58b2d2a8acf7e105c2641c0104f
Dorje <ku.dorjee@gmail.com>
2017-12-23 11:31:48 +0545

    import header and grid system in home page

--- src/components/home_page.js.jsx
+++ src/components/home_page.js.jsx
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 import '../style/App.css';
 import '../style/ideaform.css'
 import '../style/ideas.css'
-
+import Header from './header.js.jsx'
+import Sidebar from './sidebar.js.jsx'
 import IdeasComponent from './ideas_component.js.jsx'
+import {Grid, Row, Col} from 'react-bootstrap'
+
 class HomePage extends Component {
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">IdeaBroad</h1>
-          <h1 className="App-sub-title">Write your idea on IdeaBroad</h1>
-        </header>
-
-       <IdeasComponent key = {IdeasComponent.id}/>
+        <Header />
+        <Grid >
+          <Row>
+            <Col md={1} xs={4}>
+              <Sidebar />
+            </Col>
+            <Col md={11} xs={8} >
+              <IdeasComponent key = {IdeasComponent.id}/>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
