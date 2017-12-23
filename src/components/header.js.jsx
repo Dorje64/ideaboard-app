c854425c0803e10675105fdf8270adeb7473fccd
Dorje <ku.dorjee@gmail.com>
2017-12-23 11:33:16 +0545

    navbar header to the app

---
+++ src/components/header.js.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Navbar} from 'react-bootstrap'
+
+const Header = () =>
+  <Navbar collapseOnSelect>
+    <Navbar.Header>
+      <Navbar.Brand>
+        <a href="#">IdeaBroad</a>
+      </Navbar.Brand>
+    </Navbar.Header>
+  </Navbar>
+
+export default Header
