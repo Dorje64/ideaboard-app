3709d25991d2d29b6083421c1c9135cfb2197419
Dorje <ku.dorjee@gmail.com>
2017-12-24 19:15:43 +0545

    add different sidebars

--- src/components/home_page.js.jsx
+++ src/components/home_page.js.jsx
@@ -4,7 +4,8 @@ import '../style/App.css';
 import '../style/ideaform.css'
 import '../style/ideas.css'
 import Header from './header.js.jsx'
-import Sidebar from './sidebar.js.jsx'
+import SidebarLeft from './sidebar_left.js.jsx'
+import SidebarRight from './sidebar_right.js.jsx'
 import IdeasComponent from './ideas_component.js.jsx'
 import {Container, Row, Col} from 'reactstrap'
 
@@ -17,13 +18,13 @@ class HomePage extends Component {
         <Container fluid>
           <Row>
             <Col md={2} xs={2} className="sidebar-left">
-              <Sidebar />
+              <SidebarLeft />
             </Col>
             <Col md={8} xs={8} >
               <IdeasComponent key = {IdeasComponent.id}/>
             </Col>
             <Col md={2} xs={4} className="sidebar-right">
-              <Sidebar />
+              <SidebarRight />
             </Col>
           </Row>
         </Container>
