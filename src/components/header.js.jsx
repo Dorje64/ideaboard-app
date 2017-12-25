45f0bdefafd567e2eefc4116e031db24647d0dc9
Dorje <ku.dorjee@gmail.com>
2017-12-25 15:07:36 +0545

    add logout button

--- src/components/header.js.jsx
+++ src/components/header.js.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
+import {Navbar, Row, Col} from 'react-bootstrap'
+import LogOut from './log_out.js.jsx'
 
 const Header = () =>
   <Navbar className="my-navbar">
-    <Navbar.Header>
-      <Navbar.Brand className="brand-name">
-        <a href="#">IdeaBroad</a>
-      </Navbar.Brand>
-    </Navbar.Header>
+    <Row>
+    <Col md={2}>
+      <Navbar.Header>
+        <Navbar.Brand className="brand-name">
+          <a href="#">IdeaBroad</a>
+        </Navbar.Brand>
+      </Navbar.Header>
+    </Col>
+    <Col md={{size:2, offset:9}}>
+      <LogOut/>
+    </Col>
+    </Row>
+
   </Navbar>
 
 export default Header
