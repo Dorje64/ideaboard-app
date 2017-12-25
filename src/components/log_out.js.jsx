92155b77e2e6bb373a8b574529e63a848fd05d31
Dorje <ku.dorjee@gmail.com>
2017-12-25 15:08:12 +0545

    implement logout logic

---
+++ src/components/log_out.js.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom'
+
+class LogOut extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      redirect: false
+    }
+  }
+
+  handleLogout = () => {
+    window.localStorage.clear();
+    this.setState({redirect: true})
+  }
+
+  render(){
+
+    if(this.state.redirect){
+      return <Redirect to='/' />
+    }else{
+      return <button onClick={this.handleLogout}>LogOut</button>
+    }
+
+  }
+}
+
+export default LogOut
