5c0da7fd875e4403636a43fa710b6e347b0f8845
Dorje <ku.dorjee@gmail.com>
2017-12-25 13:34:42 +0545

    redirect to root after loged in

--- src/components/sign_in.js.jsx
+++ src/components/sign_in.js.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import {Container, Row, Col, Card, CardHeader, CardText,CardBody} from 'reactstrap'
 import Axios from 'axios'
-import Localstorage from 'reactjs-localstorage'
+import {Redirect} from 'react-router-dom'
+import {reactLocalStorage as LocalStorage} from 'reactjs-localstorage'
 
 export default class SignIn extends Component{
   constructor(props){
     super(props)
     this.state = {
       email: '',
-      password: ''
+      password: '',
+      redirect: false
     }
   }
 
@@ -25,35 +27,48 @@ export default class SignIn extends Component{
         password: this.state.password
       })
     .then( response => {
-      Localstorage.setItem('token', response.headers)
+      LocalStorage.setObject('tokens', response.headers);
+      this.setState({redirect: true})
     })
-    .catch( error => console.log('invalid email or password'));
+    .catch( error => {
+      console.log('Invalid Email or Password')
+      console.log(error)
+      this.setState({redirect: false})
+
+    });
   }
 
   render(){
-    return(
-      <Container>
-        <Row>
-          <Col md={{size: 4, offset: 4}}>
-            <Card className="ib-auth-container">
-              <CardHeader className="card-header-fixed">
-                <CardText tag="h3"> Login </CardText>
-              </CardHeader>
-              <form onSubmit= {this.handleSubmit}>
-                <Row>
-                  <Col md={{size:10, offset:1}}>
-                    <CardBody>
-                      <input type="text" name="email" className= "form-control ib-auth-field" placeholder="Email" onChange={this.handleInput}/>
-                      <input type="password" name="password" className = "form-control ib-auth-field" placeholder="Password" onChange={this.handleInput}/>
-                      <input type="submit" className="btn idea-button ib-auth" value="Login" />
-                    </CardBody>
-                  </Col>
-                </Row>
-              </form>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    )
+
+    if( this.state.redirect ){
+      return(<Redirect to="/" />)
+    }else{
+      return(
+        <Container>
+          <Row>
+            <Col md={{size: 4, offset: 4}}>
+              <Card className="ib-auth-container">
+                <CardHeader className="card-header-fixed">
+                  <CardText tag="h3"> Login </CardText>
+                </CardHeader>
+                <form onSubmit= {this.handleSubmit}>
+                  <Row>
+                    <Col md={{size:10, offset:1}}>
+                      <CardBody>
+                        <input type="text" name="email" className= "form-control ib-auth-field" placeholder="Email" onChange={this.handleInput}/>
+                        <input type="password" name="password" className = "form-control ib-auth-field" placeholder="Password" onChange={this.handleInput}/>
+                        <input type="submit" className="btn idea-button ib-auth" value="Login" />
+                      </CardBody>
+                    </Col>
+                  </Row>
+                </form>
+              </Card>
+            </Col>
+          </Row>
+        </Container>)
+
+    }
+
+
   }
 }
