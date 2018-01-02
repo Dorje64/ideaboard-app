bf272fa963a29a9e226209c808f3653dba25ff80
Dorje <ku.dorjee@gmail.com>
2018-01-02 11:29:25 +0545

    make chatable component impliment chat-element set listner make chat ui

--- src/components/conversation.js.jsx
+++ src/components/conversation.js.jsx
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import Axios from 'axios'
 import {reactLocalStorage as LocalStorage} from 'reactjs-localstorage';
+import { MessageList,Input } from 'react-chat-elements';
+import 'react-chat-elements/dist/main.css';
+
+const MESSAGESERVER = 'http://localhost:3001/api/v1/messages'
 
 export default class Conversation extends Component{
   constructor(props){
@@ -9,7 +13,9 @@ export default class Conversation extends Component{
       messages: [],
       isTyping: false,
       textToSent: ''
-    }
+    };
+    this.currentUid = LocalStorage.getObject('tokens').uid;
+    this.currentUserId = LocalStorage.getObject('data').id;
   }
 
   handleInput = (e) =>{
@@ -17,9 +23,9 @@ export default class Conversation extends Component{
   }
 
   renderMessages = (conversation_id) => {
-    const {uid} = LocalStorage.getObject('tokens')
-    Axios.get('http://localhost:3001/api/v1/messages',
-        {params: { conversation_id: conversation_id, uid: uid }}
+
+    Axios.get(MESSAGESERVER,
+        {params: { conversation_id: conversation_id, uid: this.currentUid }}
       )
     .then( response => {
       console.log(response.data)
@@ -29,21 +35,20 @@ export default class Conversation extends Component{
 
   componentDidMount(){
     const {id} = this.props
-    this.renderMessages(id)
+    setInterval( () => {this.renderMessages(id)} , 1000)
   }
 
-  componentWillReceiveProps(nextProps){
-    const {id} = nextProps
-    this.renderMessages(id)
-  }
+  // componentWillReceiveProps(nextProps){
+  //   const {id} = nextProps
+  //   this.renderMessages(id)
+  // }
 
   sendMessage = (e) =>{
     e.preventDefault();
     const {textToSent} = this.state;
     const {id} = this.props
-    const {uid} = LocalStorage.getObject('tokens')
-    Axios.post('http://localhost:3001/api/v1/messages',
-        { uid: uid,
+    Axios.post(MESSAGESERVER,
+        { uid: this.currentUid,
           body: textToSent,
           conversation_id: id
         }
@@ -57,16 +62,28 @@ export default class Conversation extends Component{
   }
 
   render(){
+    const currentUserId = this.currentUserId;
     return(
-      <div>
-        <h1> Conversation {this.props.id} </h1>
-        <div className="messageContainer">
-          {this.state.messages.map( message =>
-              <li key={message.id}> {message.body} </li>
-           )}
-         </div>
-        <form onSubmit={this.sendMessage}><input name="textToSent" value = {this.state.textToSent} onChange = {this.handleInput} className= "form-control ib-auth-field" /></form>
-      </div>
-    )
+      <div> <h1> Converstation </h1>
+            <MessageList
+                className='messageContainer message-list'
+                lockable= {true}
+                toBottomHeight={'100%'}
+                dataSource={
+                      this.state.messages.map( message =>(
+                        {
+                            position: currentUserId === message.sender_id ? 'right' : 'left',
+                            type: 'text',
+                            text: message.body,
+                            date: new Date(message.created_at)
+                        })
+                      )
+                    }
+          />
+      <form onSubmit={this.sendMessage}>
+        <input name="textToSent" value = {this.state.textToSent} onChange = {this.handleInput} className= "form-control ib-auth-field" />
+      </form>
+    </div>
+  )
   }
 }
