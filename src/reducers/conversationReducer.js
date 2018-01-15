1a6345c50dcef30f97bc323fb73f77504ed62543
Dorje <ku.dorjee@gmail.com>
2018-01-15 15:37:40 +0545

    dummy reducer for converstaions

---
+++ src/reducers/conversationReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  conversation_id: 1,
+  users: [1,2]
+}
+
+const conversationReducer = (state = initialState, action) => {
+  // console.log("testing dispatch conversations");
+  // console.log(state);
+  // console.log(state,action);
+  return state;
+}
+
+export default conversationReducer;
