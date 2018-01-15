0d848bc1dd19553da948c23d3951dd3f656107b1
Dorje <ku.dorjee@gmail.com>
2018-01-15 15:34:51 +0545

    import store and wrap app by provider

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css';
+import {Provider} from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+      <Provider store = {store}>
+        <App />
+      </Provider>, document.getElementById('root'));
 registerServiceWorker();
