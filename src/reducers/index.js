90039e334fffc0319a2726a5d80e9fdcfd6ec6d1
Dorje <ku.dorjee@gmail.com>
2018-01-23 15:22:21 +0545

    fix typo error

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,5 @@ import {combineReducers} from 'redux';
 import ideaReducer from './ideaReducer.js'
 import conversationReducer from './conversationReducer.js'
 
-const rootReducer = combineReducers({idea: ideaReducer, converstation: conversationReducer})
+const rootReducer = combineReducers({idea: ideaReducer, conversation: conversationReducer})
 export default rootReducer
