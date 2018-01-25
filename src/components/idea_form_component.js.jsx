517adb63af1ce388c10fcb28b0a06d1baaaa5b8d
Dorje <ku.dorjee@gmail.com>
2018-01-25 11:24:19 +0545

    remove commented code

--- src/components/idea_form_component.js.jsx
+++ src/components/idea_form_component.js.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import Axios from 'axios';
 import {Card, CardHeader, CardBody, CardFooter} from 'reactstrap';
-const IDEA_SERVER = 'http://localhost:3001/api/v1/ideas/';
 
 class IdeaFormComponent extends Component {
   constructor(props) {
@@ -22,16 +20,7 @@ class IdeaFormComponent extends Component {
       title: this.state.title,
       body: this.state.body
     }
-
     this.props.updateIdeas(this.props.idea.id,idea)
-    // Axios.put(IDEA_SERVER + String(this.props.idea.id),
-    //         {idea: idea}
-    //       )
-    //       .then(response => {
-    //         console.log(response)
-    //         this.props.updateIdeas(response.data)
-    //       })
-    //       .catch(error => console.log(error))
   }
 
   render() {
